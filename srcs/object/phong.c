6e87333248891a03a3a1ec255c4fd9c1f8bed1fc
Yeeun Park <anna862700@gmail.com>
2023-05-24 18:53:00 +0900

    feat: 난반사(diffuse) 구현

--- srcs/object/phong.c
+++ srcs/object/phong.c
@@ -1,7 +1,24 @@
 #include "../../include/main.h"
 #include "../../include/utils.h"
 
-t_color_ratio	apply_ambient(t_lights lights)
+t_color	apply_diffuse(t_lights lights, t_hitted record)
+{
+	double		diffuse;
+	t_vector	light_vector;
+	t_color	light_color;
+
+	light_vector = vector_minus(lights.light.origin, record.p);
+	light_vector = get_unit_vector(light_vector);
+	diffuse = vector_inner_product(light_vector, record.normal);
+	if (diffuse < 0)
+		diffuse = 0;
+	light_color.x = (double)lights.light.color.x / 255;
+	light_color.y = (double)lights.light.color.y / 255;
+	light_color.z = (double)lights.light.color.z / 255;
+	return (vector_multiple(light_color, diffuse * lights.light.bright_ratio));
+}
+
+t_color	apply_ambient(t_lights lights)
 {
 	double	ambient;
 
@@ -11,12 +28,13 @@ t_color_ratio	apply_ambient(t_lights lights)
 
 t_color	apply_phong_model(t_lights lights, t_hitted record)
 {
-	t_color_ratio	ratio;
+	t_color	ratio;
 
 	ratio = init_vector(0, 0, 0);
 	ratio = vector_plus(ratio, apply_ambient(lights));
-	record.color.r *= ratio.x;
-	record.color.g *= ratio.y;
-	record.color.b *= ratio.z;
-	return (record.color);
+	ratio = vector_plus(ratio, apply_diffuse(lights, record));
+	ratio.x *= record.color.x;
+	ratio.y *= record.color.y;
+	ratio.z *= record.color.z;
+	return (vector_multiple(ratio, 255));
 }
